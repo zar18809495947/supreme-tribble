752476b6f346b6bee165b5cf04055a5fb8e1ab77
zar <a18502436094@163.com>
2017-10-20 17:14:05 +0800

    创建项目

---
+++ src/main/java/com/lanou/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.lanou.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author zar on 2017/10/20.
+ */
+@Controller
+public class MainController {
+    @RequestMapping(value = "/")
+    public String home() {
+        return "index";
+    }
+}
